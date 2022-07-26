e467b40ce70a25dab76de0abd4d3ef742d8a0cc8
Luiz Aires <lfaires@gmail.com>
2022-07-25 21:02:04 -0300

    daniel promise

---
+++ index.js
@@ -0,0 +1,30 @@
+function buscaCEP() {
+  // const cep = document.querySelector("input").value;
+  // const promise = axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+  // promise.then((daniel) => {
+  //   console.log("Dados do cep: ", daniel.data);
+  //   document.querySelector("#rua") = daniel.data.logradouro;
+  //   document.querySelector("#bairro") = daniel.data.bairro;
+  //   document.querySelector("#estado") = daniel.data.uf;
+  //   document.querySelector("#cidade") = daniel.data.localidade;
+  // });
+  // promise.catch((daniel) => console.log("deu ruim: ", daniel));
+  // console.log("PROMESSA: ", promise)
+  console.log("1");
+  setTimeout(() => console.log("2"), 10000);
+  console.log("3");
+  console.log("4");
+  console.log("5");
+  console.log("6");
+  console.log("7");
+  console.log("8");
+  console.log("9");
+}
+
+//clicar no botao de busca
+// solicitacao pra api
+// esperar a api responder
+// pega a resposta da api
+
+// Protocolo HTTP => GET (buscar dados) data = dados
+// Protocolo HTTP => POST (enviar dados) data = dados
